dcc811e21a83028a55bf0301c433877737d0f0c8
Bosko Kulusic <kulusicbosko@gmail.com>
2024-07-01 13:07:14 +0200

    iterating through meals in all menus

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("radiii!!");
+
+let menuContainers = document.getElementsByClassName(
+  "menu-wrapper row justify-content-md-center menu-wrapper--menu"
+);
+
+if (menuContainers.length > 0) {
+  let menuContainer = menuContainers[0];
+
+  let menuItemContainer = menuContainer.children;
+  let menuItems = menuItemContainer[0].children;
+
+  for (let i = 0; i < menuItems.length; i++) {
+    let menuItem = menuItems[i].children;
+    console.log("radiii!!meniji");
+
+    for (let i = 0; i < menuItem.length; i++) {
+      let meal = menuItem[i];
+      console.log("radiii!!deca");
+    }
+  }
+}
