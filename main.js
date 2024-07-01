7579dde0e5ae0e700f90d70df357795102185299
Bosko Kulusic <kulusicbosko@gmail.com>
2024-07-01 13:48:44 +0200

    added mutationObserver to react to changes in DOM

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 console.log("radiii!!");
 
-let menuContainers = document.getElementsByClassName(
-  "menu-wrapper row justify-content-md-center menu-wrapper--menu"
-);
+function hideLockedMenuItems() {
+  let menuContainers = document.getElementsByClassName(
+    "menu-wrapper row justify-content-md-center menu-wrapper--menu"
+  );
 
-if (menuContainers.length > 0) {
-  let menuContainer = menuContainers[0];
+  if (menuContainers.length > 0) {
+    let menuContainer = menuContainers[0];
 
-  let menuItemContainer = menuContainer.children;
-  let menuItems = menuItemContainer[0].children;
+    let menuItemContainer = menuContainer.children;
+    let menuItems = menuItemContainer[0].children;
 
-  for (let i = 0; i < menuItems.length; i++) {
-    let menuItem = menuItems[i].children;
-    console.log("radiii!!meniji");
+    for (let i = 0; i < menuItems.length; i++) {
+      let menuItem = menuItems[i].children;
+      // console.log("radiii!!meniji" + menuItem.length);
 
-    for (let i = 0; i < menuItem.length; i++) {
-      let meal = menuItem[i];
-      console.log("radiii!!deca");
+      for (let j = 0; j < menuItem.length; j++) {
+        let meal = menuItem[j];
+
+        //   console.log("radiii!!deca");
+        let isItemAvailable = !meal.classList.contains("meal--locked");
+
+        if (!isItemAvailable) {
+          console.log("radiii!!deca" + meal.classList);
+          meal.style.display = "none";
+        }
+      }
     }
   }
 }
+
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (mutation.type === "childList") {
+      hideLockedMenuItems();
+    }
+  });
+});
+
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
