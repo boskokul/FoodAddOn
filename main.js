b1f57e2265f2f79a55bb9830538c269ef44d4769
Bosko Kulusic <kulusicbosko@gmail.com>
2024-07-03 08:33:00 +0200

    improvements

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
 console.log("radiii!!");
 
 function hideLockedMenuItems() {
-  let menuContainers = document.getElementsByClassName(
-    "menu-wrapper row justify-content-md-center menu-wrapper--menu"
-  );
+  let targetElements = document.getElementsByClassName("catering__section");
+  Array.from(targetElements).forEach((targetElement) => {
+    let menuItem = targetElement.children;
 
-  if (menuContainers.length > 0) {
-    let menuContainer = menuContainers[0];
+    for (let j = 0; j < menuItem.length; j++) {
+      let meal = menuItem[j];
 
-    let menuItemContainer = menuContainer.children;
-    let menuItems = menuItemContainer[0].children;
+      let isItemAvailable = !meal.classList.contains("meal--locked");
 
-    for (let i = 0; i < menuItems.length; i++) {
-      let menuItem = menuItems[i].children;
-      // console.log("radiii!!meniji" + menuItem.length);
-
-      for (let j = 0; j < menuItem.length; j++) {
-        let meal = menuItem[j];
-
-        //   console.log("radiii!!deca");
-        let isItemAvailable = !meal.classList.contains("meal--locked");
-
-        if (!isItemAvailable) {
-          console.log("radiii!!deca" + meal.classList);
-          meal.style.display = "none";
-        }
+      if (!isItemAvailable) {
+        meal.style.display = "none";
       }
     }
-  }
+  });
 }
+
+const targetDivs = document.getElementsByClassName("catering__section");
 
 const observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutation) {
     if (mutation.type === "childList") {
-      hideLockedMenuItems();
+      if (Array.from(targetDivs).some((div) => div.contains(mutation.target)))
+        hideLockedMenuItems();
     }
   });
 });
@@ -42,3 +32,10 @@ observer.observe(document.documentElement, {
   childList: true,
   subtree: true,
 });
+
+// Array.from(targetDivs).forEach((div) => {
+//   observer.observe(div, {
+//     childList: true,
+//     subtree: true,
+//   });
+// });
