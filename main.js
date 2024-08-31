aaaa77f6d5eb1fa4677a0ebb1bed12484c154353
Bosko Kulusic <kulusicbosko@gmail.com>
2024-08-31 07:14:26 +0200

    refactor: removed comments and console logs

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("radiii!!");
-
 function hideLockedMenuItems() {
   let targetElements = document.getElementsByClassName("catering__section");
   Array.from(targetElements).forEach((targetElement) => {
@@ -48,7 +46,6 @@ function setListenersLeftRightButtons() {
   leftButton.addEventListener("click", function () {
     moveDessertsBack();
     setTimeout(function () {
-      console.log("KliknutoL!");
       spisak = [];
       button.disabled = false;
     }, 500);
@@ -57,7 +54,6 @@ function setListenersLeftRightButtons() {
   rightButton.addEventListener("click", function () {
     moveDessertsBack();
     setTimeout(function () {
-      console.log("KliknutoL!");
       spisak = [];
       button.disabled = false;
     }, 500);
@@ -69,9 +65,7 @@ function extractDesserts() {
   let targetElements = document.querySelectorAll(".catering__section");
   let currentTypeOfMeal = "";
   let targetArray = Array.from(targetElements);
-  console.log(targetArray.length);
   for (let i = 0; i < targetArray.length; i++) {
-    // if (i === 5 || i === 2) {
     let menuItem = targetArray[i].children;
 
     for (let j = 1; j < menuItem.length; j++) {
@@ -87,9 +81,7 @@ function extractDesserts() {
           currentTypeOfMeal === "Palačinke" ||
           currentTypeOfMeal === "Ovsene kaše") &&
         meal.children.length === 3
-        // || true
       ) {
-        // console.log(meal.children.length);
         let item = {
           mealNode: meal,
           x: i,
@@ -97,15 +89,11 @@ function extractDesserts() {
           pushedBack: false,
         };
         spisak.push(item);
-        // targetArray[i].removeChild(item.mealNode);
       }
-      // }
     }
   }
-  console.log(spisak);
 
   setTimeout(function () {
-    console.log("SSSSSSSSSSSSSSSSSSSSSSS");
     let newDiv = document.createElement("div");
     newDiv.textContent = "OVO SU SVI KOLACI";
     newDiv.style.padding = "10px 0 10px 0";
@@ -122,10 +110,8 @@ function moveDessertsBack() {
   let targetElements = document.querySelectorAll(".catering__section");
   let targetArray = Array.from(targetElements);
   for (let i = 0; i <= targetArray.length - 1; i++) {
-    // for (let j = 1; j < menuItem.length; j++) {
     spisak.forEach((item) => {
       if (item.x === i && item.pushedBack === false) {
-        console.log(item.x, item.y);
         item.pushedBack = true;
         targetArray[i].appendChild(item.mealNode);
       }
@@ -134,23 +120,19 @@ function moveDessertsBack() {
       let menuItem = targetArray[i].children;
       for (let j = 1; j < menuItem.length; j++) {
         let meal = menuItem[j];
-        // console.log("EHEEHEHHH");
         if (meal.textContent === "OVO SU SVI KOLACI") {
           meal.style.display = "none";
         }
       }
     }
-    // }
   }
 }
 
 var button = document.createElement("button");
 function addButtonToPage() {
-  console.log("Button!");
   button.textContent = "Prikazi na dnu sve kolace izdvojeno";
   button.style.margin = "0 15px 10px 15px";
   button.addEventListener("click", function () {
-    console.log("Kliknuto!");
     setTimeout(function () {
       extractDesserts();
     }, 2000);
@@ -159,7 +141,6 @@ function addButtonToPage() {
   });
 
   let leftNavBar = document.querySelector(".catering__nav");
-  console.log(leftNavBar);
   if (leftNavBar.firstChild) {
     leftNavBar.insertBefore(button, leftNavBar.firstChild);
   } else {
